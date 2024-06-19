57e6dbdc160d6d927f75cdfd12fc060a8ae04f90
JuanSRovira <narukankid@gmail.com>
2024-06-19 04:03:08 -0600

    ✨ Crear usuarios

--- controllers/registerControllers.js
+++ controllers/registerControllers.js
@@ -1,21 +1,43 @@
+const jwt = require ('jsonwebtoken')
+const bcrypt = require ('bcryptjs')
+const asyncHandler = require ('express-async-handler')
+const User = require ('../models/userModel')
+
 //registrar un usuario nuevo
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error('faltan datos, por favor verifica')
+    }
+
+    //El usuario existe?
+    const userExist = await User.findOne({ email })
+    if (userExist) {
+        res.status(400) 
+        throw new Error ('El usuario ya existe')
+    } else {
+        //hash password
+    }
+
     res.status(201).json({
         msg: "usuario creado"
     })
-}
+})
 //usuario ya existente
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.status(201).json({
         msg: "usuario logeado"
     })
-}
+})
 //mostrar los datos del usuario
-const datosUser = (req, res) => {
+const datosUser = asyncHandler(async (req, res) => {
     res.status(201).json({
         msg: "datos del usuario"
     })
-}
+})
 
 module.exports = {
     registerUser,
