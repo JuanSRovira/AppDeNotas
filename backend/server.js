ff4a594f95daecccb5bc509e708ced5cb5db3e82
JuanSRovira <narukankid@gmail.com>
2024-06-26 10:14:56 -0600

    ✨ Cors para conectar la BD

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { errorHandler } = require('../middlewares/errorMiddleware')
 const colors = require ('colors')
 const connectDB = require('../config/bd')
 const port = process.env.PORT || 5000 // Esto especifica el puerto de servidor local
+const cors = require('cors')
 
 connectDB()
 
@@ -11,6 +12,7 @@ const app = express() //Especifica la naturaleza de nuestra aplicacion
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.use('/api/tareas', require('../routes/tareas.routes'))
 app.use('/api/users', require('../routes/user.routes'))
